8bb4f371a2616ff962ae11bc87eab1cf5caac756
Sam Thomas <imsamthomas@users.noreply.github.com>
2017-09-06 10:08:28 +0700

    Update registration.php

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magento_Checkout',
+    'Mageplaza_GoogleTagManager',
     __DIR__
 );
